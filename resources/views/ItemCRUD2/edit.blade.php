4bd2b5e8f286906f106d502fb3c43d27bd098bca
gunqwn <alternativewaygrup@gmail.com>
2017-02-27 10:40:11 +0800

    Commit Pertama, Versi 1

---
+++ resources/views/ItemCRUD2/edit.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+	<div class="row">
+	    <div class="col-lg-12 margin-tb">
+	        <div class="pull-left">
+	            <h2>Update Item</h2>
+	        </div>
+	        <div class="pull-right">
+	            <a class="btn btn-primary" href="{{ route('itemCRUD2.index') }}"> Back</a>
+	        </div>
+	    </div>
+	</div>
+	@if (count($errors) > 0)
+		<div class="alert alert-danger">
+			<strong>Whoops!</strong> There were some problems with your input.<br><br>
+			<ul>
+				@foreach ($errors->all() as $error)
+					<li>{{ $error }}</li>
+				@endforeach
+			</ul>
+		</div>
+	@endif
+	{!! Form::model($item, ['method' => 'PATCH','route' => ['itemCRUD2.update', $item->id]]) !!}
+	<div class="row">
+		<div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Judul:</strong>
+                {!! Form::text('title', null, array('placeholder' => 'Title','class' => 'form-control')) !!}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Deskripsi:</strong>
+                {!! Form::textarea('description', null, array('placeholder' => 'Description','class' => 'form-control','style'=>'height:100px')) !!}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+				<button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+	</div>
+	{!! Form::close() !!}
+@endsection
---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+	<h1>Anda tidak mempunyai Hak Akses</h1>
+@endsection
